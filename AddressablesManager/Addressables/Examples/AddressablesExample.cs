fc4413b0eb91359a59c2ef167217ee7137cac4c0
Max <tehmaxi@gmail.com>
2019-11-22 12:40:09 -0400

    Added files

---
+++ AddressablesManager/Addressables/Examples/AddressablesExample.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+namespace AddressablesManagement
+{
+    public class AddressablesExample : MonoBehaviour
+    {
+        public GameObject loneGameObject;
+        public GameObject[] testObjects = new GameObject[20];
+        public List<Material> materials;
+        Sprite sprite;
+        Scene sceneToLoad;
+        private Vector3 startingPos;
+
+        void Start()
+        {
+            materials = new List<Material>(3);
+
+            startingPos = transform.position;
+
+            //LoadScene("SceneTest");
+            //InstantiateSingleObject("TestObject");
+            TestInstantiateObjects();
+            //TestLoadByLabel();
+        }
+
+        async void TestLoadByLabel()
+        {
+            materials = await AddressablesManager.Instance.LoadAssetsByLabel<Material>("materials");
+        }
+
+        async void TestInstantiateObjects()
+        {
+            Vector3 pos = startingPos;
+
+            for (int i = 0; i < testObjects.Length; i++)
+            {
+                testObjects[i] = await AddressablesManager.Instance.InstantiateGameObject("TestObject");
+                testObjects[i].transform.position = pos;
+                pos += new Vector3(2f, 0, 0);
+            }
+        }
+
+        async void InstantiateSingleObject(string path)
+        {
+            loneGameObject = await AddressablesManager.Instance.InstantiateGameObject(path);
+        }
+
+        async void LoadSprite(string path)
+        {
+            sprite = await AddressablesManager.Instance.Load<Sprite>(path);
+        }
+
+        async void LoadScene(string sceneName)
+        {            
+            sceneToLoad = await AddressablesManager.Instance.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+
+        async void OnlyLoadScene(string sceneName)
+        {
+            await AddressablesManager.Instance.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+
+        async void UnloadScene(SceneInstance scene)
+        {
+            await AddressablesManager.Instance.UnloadScene(scene);
+        }
+    }
+}
---
+++ AddressablesManager/Extensions/IAsyncOperationExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+/// <summary>
+/// Script cleaned up and taken from https://forum.unity.com/threads/async-await-support-for-loading-assets.538898/
+/// </summary>
+public static class IAsyncOperationExtensions
+{
+    public static AsyncOperationAwaiter GetAwaiter(this AsyncOperationHandle operation)
+    {
+        return new AsyncOperationAwaiter(operation);
+    }
+
+    public static AsyncOperationAwaiter<T> GetAwaiter<T>(this AsyncOperationHandle<T> operation) where T : class
+    {
+        return new AsyncOperationAwaiter<T>(operation);
+    }
+
+    public readonly struct AsyncOperationAwaiter : INotifyCompletion
+    {
+        readonly AsyncOperationHandle _operation;
+
+        public AsyncOperationAwaiter(AsyncOperationHandle operation)
+        {
+            _operation = operation;
+        }
+
+        public bool IsCompleted => _operation.Status != AsyncOperationStatus.None;
+
+        public void OnCompleted(Action continuation) => _operation.Completed += (op) => continuation?.Invoke();
+
+        public object GetResult() => _operation.Result;
+
+    }
+
+    public readonly struct AsyncOperationAwaiter<T> : INotifyCompletion where T : class
+    {
+        readonly AsyncOperationHandle<T> _operation;
+
+        public AsyncOperationAwaiter(AsyncOperationHandle<T> operation)
+        {
+            _operation = operation;
+        }
+
+        public bool IsCompleted => _operation.Status != AsyncOperationStatus.None;
+
+        public void OnCompleted(Action continuation) => _operation.Completed += (op) => continuation?.Invoke();
+
+        public T GetResult() => _operation.Result;
+    }
+}
